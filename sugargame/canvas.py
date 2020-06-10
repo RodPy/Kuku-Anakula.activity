d3640009d96f01141935b71a5ea1f71e82693cfc
Rodney <rrojas@paraguayeduca.org>
2020-06-10 14:30:59 -0400

    Port Gtk +3
    replace import event

--- sugargame/canvas.py
+++ sugargame/canvas.py
@@ -2,11 +2,9 @@ import os
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-
 from gi.repository import GObject
 import pygame
-import event
-
+import sugargame.event as event
 CANVAS = None
 
 class PygameCanvas(Gtk.EventBox):
