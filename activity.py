e5c51e736172eb36ce16d70e04e605b229e67124
Rodney <rrojas@paraguayeduca.org>
2020-06-11 17:11:37 -0400

    Update activity.py

--- activity.py
+++ activity.py
@@ -22,22 +22,21 @@
 # Julius B. Lucks <julius@younglucks.com>
 # Alan Aguiar <alanjas@gmail.com>
 
-
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-
 import sugargame.canvas
+import PygameCanvas
+import kuku
+import gi
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
 from sugar3.activity import activity
 from gettext import gettext as _
-
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityToolbarButton
 from sugar3.activity.widgets import StopButton
 from sugar3.graphics.toolbarbox import ToolbarButton
 from sugar3.graphics import style
-
-import kuku
 
 class Activity(activity.Activity):
 
